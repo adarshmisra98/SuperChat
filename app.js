7f733adf29d35e1fcdc4eadb06b3fc7b8dc988f1
Adarsh <adarshmisra98@gmail.com>
2019-09-29 09:52:07 +0530

    Intital commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var app = require('express'),
+server = require('http').createServer(app),
+io = require('socket.io').listen(server),
+ent = require('ent'),
+fs = require('fs');
+
+// Loading the page index.html
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+  });
+
+io.sockets.on('connection',function(socket,username){
+     // When the username is received it’s stored as a session variable and informs the other people
+     socket.on('new_client',function(username){
+         username = ent.encode(username);
+         socket.username = username;
+         socket.broadcast.emit('new_client', username);
+     })
+})
+ // When a message is received, the client’s username is retrieved and sent to the other people
+ socket.on('message', function(message){
+     message = ent.encode(message);
+     socket.broadcast.emit('message', {username: socket.username,message:message});
+ })
+ 
